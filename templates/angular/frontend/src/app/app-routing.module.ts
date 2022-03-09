8c7e46414bad16c732084f59fbafe22fac479179
sd-hypredge <sudeep.das@hypredge.com>
2022-03-08 17:33:34 -0800

    Added angular starter

---
+++ templates/angular/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GreetComponent } from './greet/greet.component';
+
+const routes: Routes = [
+  { path: 'greet', component: GreetComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
