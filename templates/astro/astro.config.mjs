fa407f6822825bfcb7842f4ac6701e011bef0a71
Doug Moscrop <doug.moscrop@gmail.com>
2022-03-19 14:35:13 -0400

    Astro example

---
+++ templates/astro/astro.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // pages: './src/pages', // Path to Astro components, pages, and data
+  dist: './static',
+  buildOptions: {
+    site: process.env.CLOUD_URL,
+    sitemap: true,         // Generate sitemap (set to "false" to disable)
+  },
+  devOptions: {
+    // hostname: 'localhost',  // The hostname to run the dev server on.
+    // port: 3000,             // The port to run the dev server on.
+  },
+  renderers: [],
+};
