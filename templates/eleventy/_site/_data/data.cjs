bf8cdd97dd3e4d95434ebe2d2d419b5312282fe0
Jeremy Daly <jeremydaly@users.noreply.github.com>
2022-03-24 17:02:09 -0400

    update 11ty

--- templates/eleventy/_site/_data/data.cjs
+++
@@ -1,7 +0,0 @@
-const { data } = require("@serverless/cloud");
-
-// This script demostrates querying in local dev and at build time
-module.exports = async function () {
-  let results = await data.get("user:*");
-  return results.items.length > 0 ? results.items : [];
-};
