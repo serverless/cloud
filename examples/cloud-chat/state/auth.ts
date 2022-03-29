45d824dc93fbede1d372776cfa8ff97b76be285a
Russell Schick <rschick@bellwoods.ca>
2022-03-29 09:34:47 -0700

    Update cloud-chat to use native Next.js

---
+++ examples/cloud-chat/state/auth.ts
@@ -0,0 +1,100 @@
+import { proxy } from "valtio";
+
+class Auth {
+  token: string
+  user;
+  isAuthenticated = false;
+  error;
+  position;
+  systemWarning;
+
+  async login({ username, password }) {
+    const response = await fetch('/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const { token, user, message, systemWarning } = await response.json();
+    if (token) {
+      this.token = token;
+      this.user = user;
+      this.isAuthenticated = true;
+      this.watchPosition();
+    }
+    this.error = message;
+    this.systemWarning = systemWarning;
+  }
+
+  async register({ username, password, name }) {
+    const response = await fetch('/register', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password, name }),
+    });
+
+    const { token, user, message, systemWarning } = await response.json();
+    if (token) {
+      this.token = token;
+      this.user = user;
+      this.isAuthenticated = true;
+      this.watchPosition();
+    }
+    this.error = message;
+    this.systemWarning = systemWarning;
+  }
+
+  async logout() {
+    this.token = undefined;
+    this.user = undefined;
+    this.isAuthenticated = false;
+    this.error = undefined;
+  }
+
+  getToken() {
+    return this.token;
+  }
+
+  async updatePosition() {
+    const response = await fetch('/me', {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.position),
+    });
+
+    this.user = await response.json();
+  }
+
+  watchPosition() {
+    if (!("geolocation" in navigator)) {
+      console.warn("geolocation not supported");
+      return;
+    }
+
+    navigator.geolocation.watchPosition(
+      this.geolocationSuccess.bind(this),
+      this.geolocationError.bind(this)
+    );
+  }
+
+  geolocationSuccess({ coords }) {
+    this.position = {
+      lat: coords.latitude,
+      lon: coords.longitude,
+    };
+    this.updatePosition();
+  }
+
+  geolocationError(error) {
+    console.log("geolocationError", error);
+  }
+}
+
+export default proxy(new Auth());
