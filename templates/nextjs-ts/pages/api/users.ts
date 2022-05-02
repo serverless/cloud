6e0be40f1b574980b013de6a4a39cd7fca90ce32
Doug Moscrop <doug.moscrop@gmail.com>
2022-05-02 14:06:30 -0400

    Fix nextjs-ts typings to include any for now

--- templates/nextjs-ts/pages/api/users.ts
+++ templates/nextjs-ts/pages/api/users.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { data } from "@serverless/cloud";
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   const { items } = await data.get("user:*");
   res.status(200).json({ users: items.map((item) => item.value) });
 }
